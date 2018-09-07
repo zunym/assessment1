41ba989c97febd0a95c685845fa2409810317365
zunym <zunyinmon@zunyinmon.com>
2018-09-07 10:26:23 +0800

    Set up server side

---
+++ user-registration/server/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+
+var app = express();
+
+var processEnv = process.env.PORT | 3000;
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+console.log(__dirname + "/../dist/user-registration");
+app.use(express.static(__dirname + "/../dist/user-registration"))
+
+app.post("/data", (req,res)=>{
+    console.log(req.body);
+    res.json(req.body);
+})
+
+app.listen(processEnv,()=>{
+    console.log("Listening at 3000");
+})
